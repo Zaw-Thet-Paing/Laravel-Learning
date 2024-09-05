ea1f4add8db03a7e820e65f2848417b69eb15079
Zaw Thet Paing <zawthetpaing333@gmail.com>
2024-09-05 18:05:54 +0630

    breeze auth api - category, product seeder code added

--- multi-auth-api/database/seeders/ProductSeeder.php
+++ multi-auth-api/database/seeders/ProductSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,32 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Product::create([
+            'name'=> 'Lenovo Thinkpad',
+            'price'=> 599.99,
+            'category_id'=> Category::where('name', 'Electronics')->first()->id,
+            'user_id'=> 1
+        ]);
+
+        Product::create([
+            'name'=> 'The Ghost in Love',
+            'price'=> 22.99,
+            'category_id'=> Category::where('name', 'Books')->first()->id,
+            'user_id'=> 1
+        ]);
+
+        Product::create([
+            'name'=> 'Harry Potter - The Philosopher\'s Stone',
+            'price'=> 59.99,
+            'category_id'=> Category::where('name', 'Books')->first()->id,
+            'user_id'=> 1
+        ]);
+
+        Product::create([
+            'name'=> 'Watches',
+            'price'=> 99.99,
+            'category_id'=> Category::where('name', 'Accessories')->first()->id,
+            'user_id'=> 1
+        ]);
     }
 }
